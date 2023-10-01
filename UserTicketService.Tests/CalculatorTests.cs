4602f1258d61f746ee743db6bcfa3da909aa52b1
dsank <dsank@DESKTOP-I2SQ3KE>
2023-10-01 20:18:12 +0300

    Add project files.

---
+++ UserTicketService.Tests/CalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UserTicketService.Tests
+{
+
+    public class CalculatorTests
+    {
+
+
+        [Fact]
+        public void SubtractionMustReturnNotNullValue()
+        {
+            var calculator = new Calculator();
+            Assert.True( calculator.Subraction(5, 2)==3);
+
+        }
+
+        [Fact]
+        public void DivisionMustReturnNullValue() 
+        {  
+            var calculator = new Calculator();
+
+            Assert.Throws<DivideByZeroException> (()=> calculator.Division(5,0));
+        
+        }
+
+
+        [Fact]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.True(calculator.Division(200, 10) == 20);
+        }
+
+
+        [Fact]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.True(calculator.Subraction(200, 10) == 190);
+        }
+
+    }
+}
---
+++ UserTicketService/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTicketService
+{
+  public class Calculator
+    {
+
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subraction(int a, int b)
+        {
+            return (a - b);
+        }
+
+        public int Division (int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
